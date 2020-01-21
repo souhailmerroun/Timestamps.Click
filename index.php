78a3017ec44f11f8b037fec600c1ce9c214d6bd5
Souhail M <souhail.merroun1996@gmail.com>
2020-01-21 21:56:01 +0100

    Add Google Analytics to the website

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+require "vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 if(isset($_POST['submit']))
 {
     $timestamps = $_POST['timestamps'];
@@ -124,6 +129,15 @@ if(isset($_POST['submit']))
     </div>
   </main>
   
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156697578-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '<?= getenv('GOOGLE_ANALYTICS'); ?>');
+  </script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
